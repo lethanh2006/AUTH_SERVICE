d6986d63bf85903489cb172967fc3c5c23edbf19
lethanh2006 <thanhld2006.ptit@gmail.com>
2026-08-25 09:20:47 +0700

    Chuẩn hóa ngữ cảnh request cho dịch vụ xác thực

--- src/common/interfaces/request-context.interface.ts
+++ src/common/interfaces/request-context.interface.ts
@@ -3,7 +3,6 @@ import type { Request } from 'express';
 /** Dữ liệu theo suốt một HTTP request để tracing và logging. */
 export interface RequestContext {
   requestId: string;
-  startedAt: bigint;
 }
 
 export interface RequestWithContext extends Request {
